da57d76218e469cdc530d1711e67ff4165ef4e67
marcocareddu <saldumaccu@tuta.io>
2023-06-09 16:53:33 +0200

    JS - Linked to DOM & Variables

--- js/script.js
+++ js/script.js
@@ -11,3 +11,15 @@ function generateNumbers(numbers) {
     } while (generatedNumbers.includes(randomNumber) && generatedNumbers.length !== numbers);
     return generatedNumbers;
 }
+
+// * PHASE 1
+// Link DOM elements
+const countdownElement = document.querySelector('.timer');
+const numbersContainer = document.querySelector('.numbers-container');
+const calcButton = document.querySelector('btn');
+
+// Variables
+const extractedNumbers = [];
+let toInject = '';
+const countdownSeconds = 10;
+const seconds = countdownSeconds * 1000;
