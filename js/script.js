3cce096012ca97bb443c3b359fe5dcc167905cbb
marcocareddu <saldumaccu@tuta.io>
2023-06-09 16:40:37 +0200

    JS - Added Function generateNumbers

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log('JS OK')
+
+// * Functions*********************
+// Generata random numbers array
+function generateNumbers(numbers) {
+    const generatedNumbers = [];
+    let randomNumber
+    do {
+        randomNumber = Math.floor(Math.random() * 100) + 1;
+        generatedNumbers.push(randomNumber);
+    } while (generatedNumbers.includes(randomNumber) && generatedNumbers.length !== numbers);
+    return generatedNumbers;
+}
