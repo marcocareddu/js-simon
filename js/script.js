1d41fef48e1ba099914da12d14ba7b05a847dc24
marcocareddu <saldumaccu@tuta.io>
2023-06-09 16:59:58 +0200

    JS - Added function howManyInIt

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 console.log('JS OK')
 
 // * Functions*********************
-// Generata random numbers array
+// Generate random numbers array
 function generateNumbers(numbers) {
     const generatedNumbers = [];
     let randomNumber
@@ -10,6 +10,15 @@ function generateNumbers(numbers) {
         generatedNumbers.push(randomNumber);
     } while (generatedNumbers.includes(randomNumber) && generatedNumbers.length !== numbers);
     return generatedNumbers;
+}
+
+// Function how many numbers are in array
+function howManyInIt(array1, array2) {
+    const rightNumbers = [];
+    for (let i = 0; i < array1.length; i++) {
+        if (array1.includes(array2[i])) rightNumbers.push(array2[i]);
+    }
+    return rightNumbers
 }
 
 // * PHASE 1
@@ -23,3 +32,4 @@ const extractedNumbers = [];
 let toInject = '';
 const countdownSeconds = 10;
 const seconds = countdownSeconds * 1000;
+
